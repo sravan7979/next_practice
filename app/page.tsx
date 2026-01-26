8323c6af332bd8dbaefb092c664cdd62f7c71c54
sravan7979 <msravan00007@gmail.com>
2026-01-26 17:22:31 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+const Home = () => {
+  console.log("Hi")
+  return (
+    <div className='text-5xl underline'>
+      Welcome to Next.JS
+    </div>
+  )
+}
+
+export default Home
