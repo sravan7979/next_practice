e048536ca427b9701b97b58d36eb415d6e5968d2
sravan7979 <msravan00007@gmail.com>
2026-01-28 23:31:21 +0530

    Basic setup with shadcn component imported done

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,11 @@ import React from 'react'
 const Home = () => {
   console.log("Hi")
   return (
-    <div className='text-5xl underline'>
-      Welcome to Next.JS
+    <div >
+      <section>
+        <h1 className='text-center'>The Hub for every Dev <br /> Events you cannot miss</h1>
+        <p className='text-center mt-5'>Hackactons, Meetups, and Conferences all at one Place</p>
+      </section>
     </div>
   )
 }
